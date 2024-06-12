670f79b4b3336be9befd452dd3334313e413c9e1
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-06-12 12:14:18 +0700

    eslint-disable

--- src/payload/access/admins.ts
+++ src/payload/access/admins.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line prettier/prettier
 import type { AccessArgs } from 'payload/config'
 
 import { checkRole } from '../collections/Users/checkRole'
--- src/payload/access/adminsOrLoggedIn.ts
+++ src/payload/access/adminsOrLoggedIn.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line prettier/prettier
 import type { Access, AccessArgs } from 'payload/config'
 
 import { checkRole } from '../collections/Users/checkRole'
